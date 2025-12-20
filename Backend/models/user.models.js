cbb1be5c897563331e0c76dd0a0302e551930de8
muskanpatel <muskanpatelmp07@gmail.como>
2025-12-20 21:58:32 +0530

    make user module

---
+++ Backend/models/user.models.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    socketId: {
+      type: String,
+    },
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this._id, email: this.email },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.static.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+
+const userModel = mongoose.model("User", userSchema);
+module.exports = userModel;
